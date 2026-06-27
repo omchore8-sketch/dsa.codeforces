1d8a140c0782a40a2660894bff4ec3673b4ffca8
omchore8-sketch <omchore8@gmail.com>
2026-06-27 12:45:46 +0530

    Add Theatre Square [A] from Codeforces

---
+++ 1/A - Theatre Square/solution.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    long long n, m, a;
+    cin >> n >> m >> a;
+ 
+    long long rows = (n + a - 1) / a;
+    long long cols = (m + a - 1) / a;
+ 
+    cout << rows * cols;
+ 
+    return 0;
+}
