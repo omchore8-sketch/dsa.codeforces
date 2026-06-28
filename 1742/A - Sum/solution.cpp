647bc3823f3bdc9bbd20b003995488c3056f71d1
omchore <omchore8@gmail.com>
2026-06-28 14:05:13 +0530

    Add Sum [A] from Codeforces

---
+++ 1742/A - Sum/solution.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+    while (t--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (a + b == c || a + c == b || b + c == a)
+            cout << "YES" << "
+";
+ 
+        else
+            cout << "NO" << "
+";
+    }
+    return 0;
+}
