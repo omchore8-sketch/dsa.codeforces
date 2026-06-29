5a76cfe63b0c7e5b0aca8ad0a31c4299fc3ad938
omchore <omchore8@gmail.com>
2026-06-29 16:06:27 +0530

    Add Police Recruits [A] from Codeforces

---
+++ 427/A - Police Recruits/solution.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+ 
+    int police = 0, untreated = 0;
+ 
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+ 
+        if (x == -1) {
+            if (police > 0)
+                police--;
+            else
+                untreated++;
+        } else {
+            police += x;
+        }
+    }
+ 
+    cout << untreated << endl;
+    return 0;
+}
