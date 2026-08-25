550443b5e9657d9e411700c9b9b6eb5b583d3c92
omchore <omchore8@gmail.com>
2026-08-26 01:04:33 +0530

    Add Game with Integers [A] from Codeforces

---
+++ 1899/A - Game with Integers/solution.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t, n;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        if (n % 3 == 0) {
+            cout << "Second
+";
+        }
+    else {
+        cout << "First
+";
+    }
+}
+return 0;
+}
