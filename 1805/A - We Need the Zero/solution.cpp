242995e7e3aac73b35cd3c7ea6f77a2f7b5e8de9
omchore <omchore8@gmail.com>
2026-08-26 16:38:50 +0530

    Add We Need the Zero [A] from Codeforces

---
+++ 1805/A - We Need the Zero/solution.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t = 1;
+     cin >> t;
+    while (t--) {
+        long long n;
+        cin >> n;
+        long long a[n];
+        for (int i = 0; i < n; i++)
+        cin >> a[i];
+ 
+        long long total_xor = 0;
+        for (int i = 0; i < n; i++)
+        total_xor ^= a[i];
+ 
+        if (n % 2 == 1) 
+        {
+            cout << total_xor << endl;
+        }
+    else
+    {
+        if (total_xor == 0)
+        cout << total_xor << endl;
+        else
+        cout << -1 << endl;
+    }
+}
+return 0;
+}
+ 
